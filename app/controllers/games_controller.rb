ea3eda0cd9f2e3225e256e109373c7f19532cd4d
Derek McGee <dmcgee2001@gmail.com>
2023-12-07 14:59:30 -0500

    adds index action to get data to display games

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,6 +1,6 @@
 class GamesController < ApplicationController
   def index
-    response = HTTP.get("https://api.rawg.io/api/games?key=3#{ENV["GAME_API_KEY"]}")
+    response = HTTP.get("https://api.rawg.io/api/games?key=#{ENV["GAME_API_KEY"]}")
     data = JSON.parse(response)
     render json: data
   end
