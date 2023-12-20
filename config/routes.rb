a97a845e4e77d6801f1a94e6152b2dc6ed5968e2
Derek McGee <dmcgee2001@gmail.com>
2023-12-20 16:40:17 -0500

    adds routes for all actions for games

--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,8 @@ Rails.application.routes.draw do
   # Game Routes
   get "/games-fetch" => "games#fetch_data"
   get "/games/:id" => "games#show"
+  get "/games" => "games#index"
+  post "/games" => "games#create"
+  patch "/games/:id" => "games#update"
+  delete "/games" => "games#destroy"
 end
