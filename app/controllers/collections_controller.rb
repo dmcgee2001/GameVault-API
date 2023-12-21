5ba50b0cdfa45b003bedbb359a411cc8c5648ef8
Derek McGee <dmcgee2001@gmail.com>
2023-12-21 12:34:59 -0500

    adds method actions to collections controller

--- app/controllers/collections_controller.rb
+++ app/controllers/collections_controller.rb
@@ -1,2 +1,27 @@
 class CollectionsController < ApplicationController
+  before_action :authenticate_user
+
+  def index
+    @collections = current_user.collections
+    render :index
+  end
+
+  def create
+    @collection = Collection.new(
+      user_id: current_user.id,
+      game_id: params[:game_id],
+    )
+    @collection.save
+    if @collection.valid?
+      render :show
+    else
+      render json: { errors: @collection.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    collection = current_user.collections.find_by(id: params[:id])
+    collection.delete
+    render json: { message: "Game removed from collection" }
+  end
 end
