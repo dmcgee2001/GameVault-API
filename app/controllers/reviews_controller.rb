7a1e25cd82a8ecd48f4f8f6bd7dfe74322575912
Derek McGee <dmcgee2001@gmail.com>
2024-01-04 13:45:09 -0500

    adds controller action methods for review model

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,44 @@
 class ReviewsController < ApplicationController
+  def index
+    @reviews = Review.all
+    render :index
+  end
+
+  def show
+    @review = Review.find_by(id params[:id])
+    render :show
+  end
+
+  def create
+    @review = Review.new(
+      user: current_user,
+      title: params[:title],
+      text: params[:text],
+    )
+    if @review.save
+      render :show
+    else
+      render json: { errors: @game.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    @review = Review.find_by(id params[:id])
+    @review.update!(
+      user: current_user,
+      title: params[:title] || @review.title,
+      text: pararms[:text] || @review.text,
+    )
+    if @review.valid?
+      render :show
+    else
+      render json: { errors: @review.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    review = Review.find_by(id params[:id])
+    review.delete
+    render json: { message: "Review has been successfully deleted." }
+  end
 end
