2b8c4c9b1003cf8ad5353d4f5265aef285bcaad0
Derek McGee <dmcgee2001@gmail.com>
2024-01-04 14:13:49 -0500

    migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_01_04_182148) do
+ActiveRecord::Schema[7.0].define(version: 2024_01_04_191107) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -34,7 +34,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_01_04_182148) do
   end
 
   create_table "reviews", force: :cascade do |t|
-    t.string "user"
+    t.string "user_id"
     t.string "title"
     t.text "text"
     t.datetime "created_at", null: false
